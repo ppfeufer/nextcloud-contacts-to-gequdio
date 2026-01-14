39cee21f9c9ad7f6f68565de81ac9ff4f015bef3
Peter Pfeufer <info@ppfeufer.de>
2026-01-14 13:00:52 +0100

    [RELEASE] v1.2.0
    ## [1.2.0] - 2026-01-14
    
    ### Added
    
    - Name prefix from the N property (e.g., "Dr.", "Prof.", etc.)
    - Tests
    
    ### Fixed
    
    - Some methods are static and should be decorated as such
    
    ### Changed
    
    - Made it a proper Python package
    - Normalize international prefix and remove non-numeric characters except `*`
    
    ### Removed
    
    - Obsolete functions

--- nextcloud_contacts_to_gequdio/__init__.py
+++ nextcloud_contacts_to_gequdio/__init__.py
@@ -2,6 +2,6 @@
 Init file for the nextcloud_contacts_to_gequdio package.
 """
 
-__version__ = "1.1.1"
+__version__ = "1.2.0"
 
 __github_repo__ = "https://github.com/ppfeufer/nextcloud-contacts-to-gequdio"
